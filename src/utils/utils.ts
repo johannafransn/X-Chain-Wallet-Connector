9a9fe7fce49c391c9fbf782225f4047d112c9358
Johanna Fransson <adrina0002@gmail.com>
2023-08-15 11:47:32 +0200

    add wormhole connect sdk and function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,31 @@
 import { ethers } from "ethers";
 import { ExternalProvider } from "@ethersproject/providers";
+import { Wormhole, Context, Network } from '@wormhole-foundation/connect-sdk';
+import { EvmContext } from '@wormhole-foundation/connect-sdk-evm';
+
+
+const NETWORK = Network.TESTNET;
+const contexts = {
+    [Context.EVM]: EvmContext,
+
+}
+
+
+export const wormholeTestBridge = async (pkOrProvider: string | ExternalProvider, targetChainId: number, targetPublicAddress: string): Promise<string> => {
+    const wormholeSDK = new Wormhole(NETWORK, contexts);
+    const receipt = wormholeSDK.startTransfer(
+        {
+            chain: 'ethereum',
+            address: '0x123...',
+        }, // token id (native chain and address)
+        BigInt(10), // amount
+        'ethereum', // sending chain
+        '0x789...', // sender address
+        'moonbeam', // destination chain
+        '0x789...', // recipient address on destination chain
+    )
+    return receipt
+}
 
 
 
